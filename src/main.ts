ffdafcede9e5c97db4a41828fe7f5d8d3a7799d0
KevinCondor <kevin.condor@epn.edu.ec>
2024-12-22 12:03:40 -0500

    feat: Añadir nuevos endpoints en el controlador de ingresos y permitir encabezados permitidos en CORS

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
   });
 
   await app.listen(envs.port);
