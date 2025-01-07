4826d9661271f269847291075c7fdbc729d579a6
Rafaela Robalino <rafaela.robalino@epn.edu.ec>
2025-01-07 07:42:12 -0500

    fix: Correcciones en Faqs

--- src/faqs/faqs.controller.ts
+++ src/faqs/faqs.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Post, Put, Delete, Param, Body, Query, Inject } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, Query, Inject, BadRequestException } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
+import { CreateCategoryDto } from 'src/common/dto/contentManagment/advertorials/create-category.dto';
 import { CreateFaqDto } from 'src/common/dto/contentManagment/faqs/create-faq.dto';
 import { CreateFeedbackDto } from 'src/common/dto/contentManagment/faqs/create-feedback.dto';
 import { UpdateCategoryDto } from 'src/common/dto/contentManagment/faqs/update-category.dto';
@@ -13,53 +15,73 @@ export class FaqsController {
     ) { }
 
     /** FAQs */
-    @Get()
+    @Get('all')
     async getAllFaqs() {
         return this.client.send('get_all_faqs', {});
     }
 
     @Get('search')
     async searchFaqs(@Query('query') query: string) {
+        if (!query) {
+            throw new Error('Query parameter must be a valid string');
+        }
         return this.client.send('search_faqs', { query });
     }
 
-    @Post()
+    @Post('create')
     async createFaq(@Body() createFaqDto: CreateFaqDto) {
-        return this.client.send('create_faq', {...createFaqDto});
+        if (!createFaqDto.categoryId || !createFaqDto.title || !createFaqDto.description) {
+            throw new Error('Faltan campos obligatorios en la creación del enlace');
+        }
+        return this.client.send('create_faq', { ...createFaqDto });
     }
 
-    @Put(':faqId')
+    @Put('update/:faqId')
     async updateFaq(@Param('faqId') faqId: string, @Body() updateFaqDto: UpdateFaqDto) {
-        return this.client.send('update_faq', { faqId, ...updateFaqDto });
+        if (!updateFaqDto) {
+            throw new BadRequestException('Debe proporcionar los datos de actualización.');
+        }
+        return await lastValueFrom(this.client.send('update_faq', { faqId, ...updateFaqDto }));
     }
 
-    @Delete(':faqId')
+    @Delete('delete/:faqId')
     async deleteFaq(@Param('faqId') faqId: string) {
         return this.client.send('delete_faq', { faqId });
     }
 
-    @Get(':faqId')
+    @Get('detail/:faqId')
     async getFaqById(@Param('faqId') faqId: string) {
         return this.client.send('get_faq_by_id', { faqId });
     }
 
     /** Categorías */
-    @Get('categories')
+    @Get('categories/all')
     async getAllCategories() {
         return this.client.send('get_all_categories', {});
     }
 
-    @Post('categories')
-    async createCategory(@Body() data: any) {
-        return this.client.send('create_category', data);
+    @Post('categories/create')
+    async createCategory(@Body() createCategoryDto: CreateCategoryDto) {
+        if (!createCategoryDto.name) {
+            throw new Error('El nombre de la categoría es obligatorio');
+        }
+        return this.client.send('create_category', { ...createCategoryDto });
     }
 
-    @Put('categories/:id')
+    // Actualizar una categoría existente
+    @Put('categories/update/:id')
     async updateCategory(@Param('id') id: number, @Body() updateCategoryDto: UpdateCategoryDto) {
-        return this.client.send('update_category', { id, ...updateCategoryDto });
+        const result = await this.client.send('update_category', { id, updateCategoryDto }).toPromise();
+
+        if (!result) {
+            throw new Error('No se recibió respuesta del microservicio.');
+        }
+
+        return result;
     }
 
-    @Delete('categories/:id')
+    // Eliminar una categoría
+    @Delete('categories/delete/:id')
     async deleteCategory(@Param('id') id: number) {
         return this.client.send('delete_category', { id });
     }
