375945ea6cf7c4a5ea16f23810ab5966ca4059e6
jackson-ramon <jackson.ramon@epn.edu.ec>
2025-02-03 06:44:30 -0500

    feat: Se agrega el modulo de ordenes

---
+++ src/common/dto/order/create-order.dto.ts
@@ -0,0 +1,35 @@
+import { IsBoolean, IsNumber, IsOptional, IsPositive, IsUUID, Length } from "class-validator";
+
+export class CreateOrderDto {
+    @IsUUID()
+    userId: string;
+
+    @IsNumber()
+    @IsPositive()
+    // @Type(() => Number)
+    totalItems: number;
+
+    @IsNumber()
+    @IsPositive()
+    // @Type(() => Number)
+    totalAmount: number;
+
+    // @IsBoolean()
+    // @IsOptional()
+    // isPaid: boolean = false;
+
+    @IsOptional()
+    @IsBoolean()
+    homeDelivery?: boolean;
+
+    @IsOptional()
+    @IsUUID()
+    petOwnerAddressId?: string;
+
+    @IsOptional()
+    petOwnerAddress?: string;
+
+    @IsOptional()
+    @Length(10, 10)
+    petOwnerPhone?: string;
+}
---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,42 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Inject, Query } from '@nestjs/common';
+import { CreateOrderDto, UpdateOrderDto } from '../common/index';
+import { ClientProxy } from '@nestjs/microservices';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(
+    @Inject('NATS_SERVICE') private readonly client: ClientProxy,
+  ) {}
+
+  @Post()
+  async create(@Body() createOrderDto: CreateOrderDto) {
+    return this.client.send('create_order', createOrderDto);
+  }
+
+  @Get()
+  async findAllByUser(@Query('userId') userId: string) {
+    return this.client.send('get_all_user_orders', {userId});
+  }
+
+  @Patch(':id')
+  async orderPaid(@Param('id') id: string, @Body('userId') userId: string) {
+    const payload = {id, userId};
+    console.log('payload', payload);
+    return this.client.send('order_paid', payload);
+  }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.client.send('find_order_by_id', id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
+  //   return this.client.send('update_order', { id, ...updateOrderDto });
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.client.send('delete_order', id);
+  // }
+}
